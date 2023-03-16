0de064f1d6eb1b3e529629734afc0b25bf824518
parulpatel3380 <parulpatel3380@gmail.com>
2023-03-16 20:49:37 +0000

    java-week1-writing-hw-parul

---
+++ src/homework_week_5/Programme_1_BioData.java
@@ -0,0 +1,21 @@
+package homework_week_5;
+
+
+public class Programme_1_BioData {
+    public static void main(String[] args) {
+        System.out.println("              BIO DATA          ");
+        System.out.println("Name              :  Jane Smith  ");
+        System.out.println("Mobile            :  07912 345678");
+        System.out.println("Email             :  janesmith@gmial.com");
+        System.out.println("Gender            :  Female    ");
+        System.out.println("Date of Birth     :  1.05.2000");
+        System.out.println("MaritalStatus     :  Sigle");
+        System.out.println("Languages Known   :  English,Spanish and German");
+        System.out.println("Qualification     :  Software Tester");
+        System.out.println("Experience        :  Agile Tester with 5 years of experience");
+        System.out.println("Address           :  123 Abc Street, London AB1 2CD");
+        System.out.println("Place             :  London");
+        System.out.println("Date              :  02.10.2021");
+        System.out.println("Signature         :  JSmith");
+    }
+}
